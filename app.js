71cad7a4e869a85fd372c91152ea92bcca59b33d
baconstarvation <ant.ucd@gmail.com>
2014-02-17 21:21:55 -0800

    Next step: we need to render our new ingredient form

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var routes = require('./routes');
 var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+var ingredient = require('./routes/ingredient');
 
 var app = express();
 
@@ -30,6 +31,9 @@ if ('development' == app.get('env')) {
 
 app.get('/', routes.index);
 app.get('/users', user.list);
+app.get('/ingredient/new', ingredient.new);
+
+// dont forget the hw2 dependencies + the change in 'if development' and a few other housekeeping things for templating
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
---
+++ routes/ingredient.js
@@ -0,0 +1,6 @@
+
+exports.new = function (req, res) {
+	console.log("hey thar");
+	res.render('ingredient',{});
+	// res.send("hey thar");
+};
